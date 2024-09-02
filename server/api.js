8e2c8b6ee1225a4a96887d084f1b4ae31f9617a4
Jesse Vahlfors <jesse.vahlfors@gmail.com>
2024-09-02 08:26:53 +0300

    Fix routing by updating apiRouter.use in api.js

--- server/api.js
+++ server/api.js
@@ -2,12 +2,12 @@ const express = require('express');
 const apiRouter = express.Router();
 
 const minionsRouter = require('./minions')
-app.use('/minions', minionsRouter);
+apiRouter.use('/minions', minionsRouter);
 
 const ideasRouter = require('./ideas')
-app.use('/ideas', ideasRouter);
+apiRouter.use('/ideas', ideasRouter);
 
 const meetingsRouter = require('./meetings')
-app.use('/meetings', meetingsRouter);
+apiRouter.use('/meetings', meetingsRouter);
 
 module.exports = apiRouter;
